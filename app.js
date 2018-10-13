e91f05fc198c796af3fbc27db8578b1195464114
icantwritecode <andrchas@gmail.com>
2018-10-13 14:35:14 +0300

    headers

--- app.js
+++ app.js
@@ -39,7 +39,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:4000");
+  res.header("Access-Control-Allow-Origin", "*");
   res.header(
     "Access-Control-Allow-Headers",
     "Origin, X-Requested-With, Content-Type, Accept, Authorization"
